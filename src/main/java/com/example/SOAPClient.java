4d9cb6de92022f37c836613105a19964ea125d22
ryan64128 <ryan64128@yahoo.com>
2021-07-15 19:25:59 -0400

    refactored client main

--- src/main/java/com/example/SOAPClient.java
+++ src/main/java/com/example/SOAPClient.java
@@ -7,12 +7,9 @@ public class SOAPClient {
 	public static void main(String[] args) throws MalformedURLException{
 		URL url =new URL("http://localhost:8084/soap-math?wsdl");
 		SOAPMathImplService service = new SOAPMathImplService(url);
-		
 		SOAPMathOps eservice = service.getSOAPMathImplPort();
-		
-		
+		// print responses from soap server
 		System.out.println(eservice.squareIt(2));
-		
 		System.out.println(eservice.cubeIt(2));
 	}
 }
